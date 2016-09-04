be428524aa69eeb339b3dbdc9d61473d287ac1a5
James Little <jzachlittle@gmail.com>
2016-09-04 07:19:01 -0500

    updating code

--- PrimeFinder/app/src/main/java/com/zachlittle/android/aca/primefinder/MainActivity.java
+++ PrimeFinder/app/src/main/java/com/zachlittle/android/aca/primefinder/MainActivity.java
@@ -1,80 +1,110 @@
 package com.zachlittle.android.aca.primefinder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     PrimeFinder mPrimeFinder;
     Button mButton;
     TextView mTextView;
     EditText mNumInput;
-
+    long longNum;
+    String[] resultsArray;
+    int numPrimes;
+    int test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         mButton = (Button) findViewById(R.id.button);
         mTextView = (TextView) findViewById(R.id.textView);
         mNumInput = (EditText) findViewById(R.id.editText);
         mTextView.setMovementMethod(new ScrollingMovementMethod());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
 
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                long longNum = Long.parseLong(mNumInput.getText().toString());
-                mPrimeFinder = new PrimeFinder(longNum, mTextView);
-            }
+                mTextView.setText("");
+                longNum = Long.parseLong(mNumInput.getText().toString());
+                int arraySize = (int) (longNum);
+                resultsArray = new String[arraySize];
+                mPrimeFinder = new PrimeFinder(longNum);
+                while (mPrimeFinder.finished = true) {
+                    for (int i =0; i < arraySize ; i++ ) {
+                        mTextView.append(resultsArray[i]);
+                        System.out.println(resultsArray[i]);
 
+                    }
+                }
+
+            }
         });
 
 
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    public class PrimeFinder implements Runnable{
+
+
+        public long target;
+        public boolean finished = false;
+        public Thread runner;
+
+
+
+
+        PrimeFinder(long inTarget) {
+            target = inTarget;
+
+            if (runner == null){
+                runner = new Thread(this);
+                runner.start();
+            }
+        }
+        @Override
+        public void run() {
+            long candidate = 2;
+            numPrimes =0;
+            while (candidate < target) {
+                if (isPrime(candidate)) {
+                    resultsArray[numPrimes] = ("Here's a prime!:" + candidate);
+                    numPrimes++;
+
+                }
+                candidate++;
+
+            }
+            finished = true;
+
         }
 
-        return super.onOptionsItemSelected(item);
+        boolean isPrime(long checkNumber) {
+            double root = Math.sqrt(checkNumber);
+            for (int i =2; i<=root; i++) {
+                if (checkNumber % i ==0) {
+                    return false;
+                }
+            }return true;
+        }
+
+
+
     }
 }
--- PrimeFinder/app/src/main/java/com/zachlittle/android/aca/primefinder/PrimeFinder.java
+++ PrimeFinder/app/src/main/java/com/zachlittle/android/aca/primefinder/PrimeFinder.java
@@ -1,55 +1,6 @@
 package com.zachlittle.android.aca.primefinder;
 
-import android.widget.TextView;
-
 /**
  * Created by Zach on 9/2/16.
  */
-public class PrimeFinder implements Runnable{
 
-    public long prime;
-    public long target;
-    public boolean finished = false;
-    private Thread runner;
-    TextView mTextView;
-
-
-
-    PrimeFinder(long inTarget, TextView textView) {
-        target = inTarget;
-        mTextView = textView;
-        if (runner == null){
-            runner = new Thread(this);
-            runner.start();
-        }
-    }
-    @Override
-    public void run() {
-        long numPrimes = 0;
-        long candidate =2;
-
-        while (candidate < target) {
-            if (isPrime(candidate)) {
-                numPrimes++;
-                prime = candidate;
-                mTextView.append("\nPrime Number found: " + candidate);
-            }
-            candidate++;
-
-        }
-        mTextView.append("Number of Primes: " + numPrimes);
-        finished = true;
-
-    }
-
-    boolean isPrime(long checkNumber) {
-        double root = Math.sqrt(checkNumber);
-        for (int i =2; i<=root; i++) {
-            if (checkNumber % i ==0) {
-                return false;
-            }
-        }return true;
-    }
-
-
-}
