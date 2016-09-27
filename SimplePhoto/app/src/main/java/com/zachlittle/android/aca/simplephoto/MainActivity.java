d931192bb570743b71a7f3b283f825485fd8c3c3
James Little <jzachlittle@gmail.com>
2016-09-27 10:43:16 -0500

    add simple photo, update what it's app

---
+++ SimplePhoto/app/src/main/java/com/zachlittle/android/aca/simplephoto/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zachlittle.android.aca.simplephoto;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int CAMERA_REQUEST = 123;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}
--- WheresItsSnap/app/src/main/java/com/zachlittle/android/aca/wheresitssnap/CaptureFragment.java
+++ WheresItsSnap/app/src/main/java/com/zachlittle/android/aca/wheresitssnap/CaptureFragment.java
@@ -1,14 +1,36 @@
 package com.zachlittle.android.aca.wheresitssnap;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class CaptureFragment extends Fragment {
+
+    private static final int CAMERA_REQUEST = 123;
+    private ImageView mImageView;
+
+    String mCurrentPhotoPath;
+
+    private Uri mImageUri = Uri.EMPTY;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +40,75 @@ public class CaptureFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_capture, container, false);
+        View view = inflater.inflate(R.layout.capture_fragment, container, false);
+
+        mImageView = (ImageView) view.findViewById(R.id.imageView);
+        Button  btnCapture = (Button) view.findViewById(R.id.btnCapture);
+        Button btnSave = (Button) view.findViewById(R.id.btnSave);
+
+        final EditText mEditTextTitle = (EditText) view.findViewById(R.id.editTextTitle);
+        final EditText mEditTextTag1 = (EditText) view.findViewById(R.id.editTextTag1);
+        final EditText mEdditTetTag2 = (EditText) view.findViewById(R.id.editTextTag2);
+        final EditText mEditTextTag3 = (EditText) view.findViewById(R.id.editTextTag3);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                }catch (IOException ex){
+                    Log.e("error", "error creating file");
+                }
+                if (photoFile !=null){
+                    mImageUri = Uri.fromFile(photoFile);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
 
         return view;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  // filename
+                ".jpg",         // extension
+                storageDir      // folder
+        );
+
+        // Save for use with ACTION_VIEW Intent
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
+            try {
+                mImageView.setImageURI(Uri.parse(mImageUri.toString()));
+            }catch (Exception e){
+                Log.e("Error", "Uri not set");
+            }
+        }else {
+            mImageUri = Uri.EMPTY;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        BitmapDrawable bd = (BitmapDrawable) mImageView.getDrawable();
+        bd.getBitmap().recycle();
+        mImageView.setImageBitmap(null);
     }
 }
